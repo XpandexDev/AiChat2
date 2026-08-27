cc924dac6fcacf0898a8908de895395a33bcaece
Carlos Pérez Álvarez <carlosperezalvarez@MacBook-Air-de-Xpandex.local>
2026-08-27 12:20:34 +0200

    Deploy build 2026-08-27T10:20:34Z

---
+++ deploy/browser/chunk-3XMDB2VD.js
@@ -0,0 +1,1 @@
+import"./chunk-QEZ3K4DX.js";var t=[{path:"",loadComponent:()=>import("./chunk-P6JZD63U.js").then(o=>o.AuditLogComponent)}];export{t as AUDIT_ROUTES};
--- deploy/browser/chunk-DCAUL5WS.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-QEZ3K4DX.js";var t=[{path:"",loadComponent:()=>import("./chunk-2T4HY4FM.js").then(o=>o.SessionsListComponent)}];export{t as SESSIONS_ROUTES};
