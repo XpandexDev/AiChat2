ae1d69b2de0003635107b0e42c944fbcdfe1b50c
Carlos Pérez Álvarez <carlosperezalvarez@Air-de-Xpandex.station>
2026-09-02 12:36:47 +0200

    Deploy build 2026-09-02T10:36:47Z

--- deploy/browser/chunk-IH3UWJEE.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-QEZ3K4DX.js";var o=[{path:"",loadComponent:()=>import("./chunk-LBTHP5IG.js").then(t=>t.ClientDashboardComponent)},{path:"ajustes",loadComponent:()=>import("./chunk-XBTHZ6MO.js").then(t=>t.ClientSettingsComponent)}];export{o as PANEL_ROUTES};
